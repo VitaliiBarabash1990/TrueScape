4c52a89f98a83efe6f713ab85ebd60dfb1ba47d1
vitalii-pc <alievruslan1818@gmail.com>
2025-02-16 19:45:05 +0100

    intercepting routes

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,25 +1,28 @@
-'use client';
+"use client";
 
-import React, { useEffect } from 'react';
-import Header from '@/app/components/header';
-import { notFound } from 'next/navigation';
+import React, { useEffect } from "react";
+import Header from "@/app/components/header";
+import { notFound } from "next/navigation";
 export interface PageProps {
-  // params: { id: string[] };
-  params: { id: string };
+	// params: { id: string[] };
+	params: { id: string };
 }
 export default function Page({ params }: PageProps) {
-  useEffect(() => {
-    const id = Number.parseInt(params.id);
-    if (Number.isNaN(id)) {
-      notFound();
-    }
-  }, [params.id]);
+	useEffect(() => {
+		const id = Number.parseInt(params.id);
+		if (Number.isNaN(id)) {
+			notFound();
+		}
+	}, [params.id]);
 
-  return (
-    <>
-      {/* <Header>Company ({String(params.id)})</Header> */}
-      <Header>Company ({params.id})</Header>
-      <p>{new Date().toTimeString()}</p>
-    </>
-  );
+	return (
+		// <>
+		//   <Header>Company ({String(params.id)})</Header>
+		//   <Header>Company ({params.id})</Header>
+		//   <p>{new Date().toTimeString()}</p>
+		// </>
+		<div className="py-6 px-10">
+			<p>{`Information about company (${params.id})`}</p>
+		</div>
+	);
 }
