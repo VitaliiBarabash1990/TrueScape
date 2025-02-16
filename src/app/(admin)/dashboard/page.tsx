2da79c7f87da39b5081e26274c75479f00bd6d7d
vitalii-pc <alievruslan1818@gmail.com>
2025-02-16 18:55:20 +0100

    add slots

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,13 +1,17 @@
-import MagicButton from '@/app/components/magic-button';
-import React from 'react';
+import Header from "@/app/components/header";
+// import MagicButton from '@/app/components/magic-button';
+import React from "react";
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
-    </main>
-  );
+	return (
+		// <main>
+		//   <h1 className="text-xl">Dashboard Page</h1>
+		//   <MagicButton />
+		// </main>
+		<>
+			<Header>Dashboard</Header>
+		</>
+	);
 }
